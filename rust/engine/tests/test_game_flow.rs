7b0fa19f1c66bd768ce73a5d80028864914b064f
Kodai Miyashita <luenauxle.myphaxem@gmail.com>
2025-08-30 21:21:29 +0900

    test(engine): ストリート進行の赤テストを追加 (タスク4.4)

---
+++ rust/engine/tests/test_game_flow.rs
@@ -0,0 +1,14 @@
+use axm_engine::engine::Engine;
+
+#[test]
+fn deal_hand_progresses_streets_and_completes() {
+    let mut eng = Engine::new(Some(1), 1);
+    eng.shuffle();
+    eng.deal_hand().expect("deal ok");
+    let board = eng.board();
+    assert_eq!(board.len(), 5);
+    let players = eng.players();
+    assert!(players.iter().all(|p| p.hole_cards()[0].is_some() && p.hole_cards()[1].is_some()));
+    assert!(eng.is_hand_complete());
+}
+
