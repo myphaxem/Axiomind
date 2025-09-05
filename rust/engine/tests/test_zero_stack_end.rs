c7b687888a120e1347011189d29ac4bcd90f4b1f
Kodai Miyashita <luenauxle.myphaxem@gmail.com>
2025-09-05 12:00:00 +0900

    test(engine B 4.1 Red): refuse new hand [Red]

---
+++ rust/engine/tests/test_zero_stack_end.rs
@@ -0,0 +1,16 @@
+use axm_engine::engine::Engine;
+
+#[test]
+fn zero_stack_prevents_new_hand() {
+    let mut eng = Engine::new(Some(1), 1);
+    // Drain player 0 stack to zero
+    {
+        let players = eng.players_mut();
+        let s0 = players[0].stack();
+        players[0].bet(s0).expect("bet to zero");
+    }
+    // Next deal should refuse
+    let r = eng.deal_hand();
+    assert!(r.is_err(), "deal_hand should error when any stack is zero");
+}
+
